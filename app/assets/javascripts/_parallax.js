e248d5c1d1fad2030b5c1deaa45d6376d5372d01
Rob McGrail <rob.mcgrail@gmail.com>
2015-02-09 13:32:52 +1300

    Nice header for tasks

---
+++ app/assets/javascripts/_parallax.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  if ($(".parallax-window").length) {
+    parallax();
+  }
+});
+
+$(window).scroll(function(e) {
+  if ($(".parallax-window").length) {
+    parallax();
+  }
+});
+
+function parallax(){
+  if( $(".parallax-window").length > 0 ) {
+    var plxBackground = $(".parallax-background");
+    var plxWindow = $(".parallax-window");
+
+    var plxWindowTopToPageTop = $(plxWindow).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+    var windowInnerHeight = window.innerHeight;
+    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+    var plxSpeed = 0.60;
+
+    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+  }
+}
