e3033718c43eaf4b418e148deb86ecc34d906f93
Rob McGrail <rob.mcgrail@gmail.com>
2015-02-10 13:04:14 +1300

    Adding a top nav to tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,12 @@ module TasksHelper
   def complete?(task)
     task.marked_as? :complete, :by => current_user
   end
+
+  def current?(task)
+    unless defined? @task
+      return false
+    end
+
+    @task.id == task.id
+  end
 end
