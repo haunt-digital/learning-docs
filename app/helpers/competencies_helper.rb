fce6c3ae00068860d79d2262810aa203ddecfb7f
Rob McGrail <rob.mcgrail@gmail.com>
2015-02-11 17:48:56 +1300

    Dynamic badge colours based on scores

--- app/helpers/competencies_helper.rb
+++ app/helpers/competencies_helper.rb
@@ -1,22 +1,52 @@
 module CompetenciesHelper
+  def subscribed?(competency)
+    competency.subscribed_by?(current_user)
+  end
+
   def skill_completion(competency)
     complete = competency.skills_complete_for(current_user)
     total = competency.skills.count
-    "#{complete} of #{total}"
+    ratio = complete / total
+
+    if ratio.between?(0.8, 1)
+      badge_class = 'success'
+    elsif ratio.between?(0.3, 0.8)
+      badge_class = 'notice'
+    else
+      badge_class = 'alert'
+    end
+
+    "<span class='badge-#{badge_class}'>#{complete} of #{total} skills</span>".html_safe
   end
 
   def competency_score(competency)
-    "#{competency.collections_completion_score_for(current_user)} pts"
+    score = competency.collections_completion_score_for(current_user)
+
+    if score > (200)
+      badge_class = 'success'
+    elsif score.between?(80, 200)
+      badge_class = 'notice'
+    else
+      badge_class = 'alert'
+    end
+
+    "<span class='badge-#{badge_class}'>#{competency.collections_completion_score_for(current_user)} pts</span>".html_safe
   end
 
   def competency_percentage(competency)
     total = competency.tasks.count
     completed = competency.tasks_complete_for(current_user)
     percentage = completed.to_f / total.to_f * 100.0
-    number_to_percentage(percentage, precision: 0)
+
+    if percentage.between?(80, 100)
+      badge_class = 'success'
+    elsif percentage.between?(30, 80)
+      badge_class = 'notice'
+    else
+      badge_class = 'alert'
+    end
+
+    "<span class='badge-#{badge_class}'>#{number_to_percentage(percentage, precision: 0)}</span>".html_safe
   end
 
-  def subscribed?(competency)
-    competency.subscribed_by?(current_user)
-  end
 end
