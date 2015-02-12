e4777935f651fde5a440e2ebe60b68630abbab4f
Rob McGrail <rob.mcgrail@gmail.com>
2015-02-12 14:25:48 +1300

    better puma defaults

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 1)
+threads Integer(ENV['MIN_THREADS']  || 2), Integer(ENV['MAX_THREADS'] || 4)
 
 preload_app!
 
